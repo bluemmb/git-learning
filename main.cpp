4f9b6336a955cac1f6c0510e4f6dbba4a264566e
bluemmb <bluemmb22@gmail.com>
2017-11-13 11:40:50 +0330

    Move factorial to a function

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include <iostream>
 using namespace std;
 
+int fact( int n )
+{
+	int ans = 1;
+	for ( int i=1 ; i<=n ; i++ )
+		ans = ans * i;
+	return ans;
+}
+
 int main() 
 {
 	int n;
 	cin>>n;
 	
-	int ans = 1;
-	for ( int i=1 ; i<=n ; i++ )
-		ans = ans * i;
-	
-	cout<<ans;
+	cout<<fact(n);
 	return 0;
 }
 
